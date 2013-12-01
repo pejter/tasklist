2eafae58111964dc0d5caf12f7ef5c73486b3283
GrandiPL <jakub.wachowiak@gmail.com>
2013-12-01 22:35:27 +0000

    Added login ui, edited htacces for git repo, added bootstrap

--- src/index.php
+++ src/index.php
@@ -1,11 +1,45 @@
+<html lang="pl">
+<head>
+    <meta charset="iso-8859-2">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+    <title>Tasklist Login Page</title>
+
+    <link href="template/css/bootstrap.css" type="text/css" rel="stylesheet">
+    <link href="signin.css" type="text/css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Zaloguj się</h2>
+        <input type="text" class="form-control" placeholder="Adres Email" required="" autofocus="">
+        <input type="password" class="form-control" placeholder="Hasło" required="">
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me"> Zapamiętaj
+        </label>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Zaloguj</button>
+      </form>
+
+    </div> <!-- /container -->
+
+</body></html>
 <?php
 
 require 'vendor/autoload.php';
 
-$db_name = "";
-$db_host = "";
-$db_user = "";
-$db_pass = "";
+
+$db_name = "task";
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "toor";
 
 $pdo = new PDO("mysql:host=".$db_host.";dbname=".$db_name, $db_user, $db_pass);
 $db = new NotORM($pdo);
