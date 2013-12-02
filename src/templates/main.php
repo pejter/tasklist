04cb12880331e545b722e0157e00632ab88e6731
Pejter <piotrluczak1995@gmail.com>
2013-12-02 17:18:28 +0100

    Fixing project. Cleaning up

---
+++ src/templates/main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+	<title>Tasklist<?php if($title) echo ' - '.$title ?></title>
+
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
+</head>
+<body>
+	<?php echo $yield ?>
+</body>
+</html>
---
+++ src/templates/signin.php
@@ -0,0 +1,20 @@
+<style type="text/css" media="screen">
+	@import '../static/css/signin.css';
+</style>
+
+<div class="container">
+
+	<form class="form-signin">
+		<h2 class="form-signin-heading">Zaloguj sie™</h2>
+
+		<input type="text" class="form-control" placeholder="Adres Email" required="" autofocus="">
+		<input type="password" class="form-control" placeholder="Hasło" required="">
+
+		<label class="checkbox">
+			<input type="checkbox" value="remember-me"> Zapamietaj
+		</label>
+
+		<button class="btn btn-lg btn-primary btn-block" type="submit">Zaloguj</button>
+	</form>
+
+</div> <!-- /container -->
