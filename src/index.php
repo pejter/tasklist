8f7a03e5bf85733daf20fec2a8f5b58050e069ba
Pejter <piotrluczak1995@gmail.com>
2013-12-03 19:25:59 +0100

    Added user interaction

--- src/index.php
+++ src/index.php
@@ -3,6 +3,7 @@
 require 'vendor/autoload.php';
 require 'config.php'; //engine config
 define('ENGINE', 'ENGINE'); //sql engine used by the database, possible: MySQL(mysql), SQLite(sqlite), PostgreSQL(pgsql), MS SQL(sqlsrv), Oracle(oci)
+define('SESSION_TIMEOUT', 600);
 
 $pdo = new PDO(ENGINE.":host=".$db_host.";dbname=".$db_name, $db_user, $db_pass);
 $db = new NotORM($pdo);
@@ -14,10 +15,26 @@ $app = new \Slim\Slim(array(
 	'view' => '\Slim\LayoutView',
 	'layout' => './templates/main.php'
 ));
+\Slim\Route::setDefaultConditions(array(
+	'id' => '[0-9]{1,}'
+));
+$db->debug = $app->config('debug');
+
+function refresh(){
+	$app->redirect($app->request->getPath());
+}
+
+//check session timeout and logout current user
+if($_SESSION['lastRequest']+$_SESSION['timeout']<time()){
+	destroy_session();
+	refresh();
+} else {
+	$_SESSION['lastRequest'] = time();
+}
 
 
 //routes
-$app->get('/', function (){
+$app->get('/', function () use ($app,$db){
 	$tasks = $db->task();
 
 	$app->render('home.php',array(
@@ -26,15 +43,66 @@ $app->get('/', function (){
 	));
 });
 
-$app->group('/task', function () use ($app,$db) {
+//login routes
+$app->get('/login', function () use ($app){
+	if(!isset($_SESSION['username']))
+		$app->render('signin.php');
+	else {
+		$address = $app->request->get('redirect_url');
+		if($address==null)
+			$app->redirect('/');
+		else
+			$app->redirect($address);
+	}
+});
+
+$app->post('/login', function () use ($app,$db){
+	$post = $app->request->post();
+	$user = $db->user()->where('username=? AND password=?', $post['username'], md5($post['password']))->fetch();
+	if($user){
+		session_start();
+		$_SESSION['lastRequest'] = time();
+		$_SESSION['timeout'] = SESSION_TIMEOUT;
+		$_SESSION['username'] = $user['username'];
+	}
+});
+
+$app->get('/logout', function () use ($app){
+	session_destroy();
+	$address = $app->request->get('redirect_url');
+	if($address==null)
+		$app->redirect('/');
+	else
+		$app->redirect($address);
+});
+
+//register
+$app->get('/register', function () use ($app){
+	if(!isset($_SESSION['username']))
+		$app->render('register.php');
+	else
+		$app->render('/');
+});
+
+$app->post('/register', function () use ($app,$db){
+	$userInfo = $app->request->post();
+	if(!preg_match('/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i', $userInfo['email']))
+		echo "Not an email";
+	elseif($db->user()->where('user=? OR email=?'))
+		echo "User already exists!";
+	else
+		$db->user()->insert($userInfo);
+});
+
+$app->group('/task', function () use ($app) {
 
 	//task adding
-	$app->get('/add', function (){
+	$app->get('/add', function () use ($app){
 		$app->render('task-add.php');
 	});
 
-	$app->post('/add', function (){
-		$post = $app->request()->post();
+	$app->post('/add', function () use ($app,$db){
+		$post = $app->request->post();
 		$result = $db->task()->insert($post);
 
 		if(!$result)
@@ -43,8 +111,9 @@ $app->group('/task', function () use ($app,$db) {
 			echo "Task added";
 	});
 
-	$app->get('/:taskID',function ($taskID){
-		$task = $db->task()->where('id',$taskID);
+	//task details
+	$app->get('/:id',function ($id) use ($app,$db){
+		$task = $db->task()->where('id',$id);
 
 		if($task->fetch())
 			$app->render('task-details.php',array(
@@ -55,8 +124,8 @@ $app->group('/task', function () use ($app,$db) {
 	});
 
 	//task editing
-	$app->get('/:taskID/edit', function ($taskID){
-		$task = $db->task()->where(id, $taskID);
+	$app->get('/:id/edit', function ($id) use ($app,$db){
+		$task = $db->task()->where('id', $id);
 
 		if($task->fetch())
 			$app->render('task-edit.php', array(
@@ -64,11 +133,11 @@ $app->group('/task', function () use ($app,$db) {
 			));
 	});
 
-	$app->put('/:taskID/edit', function ($taskID){
-		$task = $db->task()->where(id, $taskID);
+	$app->put('/:id/edit', function ($id) use ($app,$db){
+		$task = $db->task()->where('id', $id);
 
 		if($task->fetch()){
-			$put = $app->request()->put();
+			$put = $app->request->put();
 			$result = $task->update($put);
 
 			if(!$result)
@@ -83,11 +152,13 @@ $app->group('/task', function () use ($app,$db) {
 
 $app->group('/group', function () use ($app,$db){
 
+	//group adding
 	$app->get('/add', function (){
 		echo "adding group";
 	});
 
-	$app->get('/:groupID',function ($groupID){
+	//group details
+	$app->get('/:id',function ($id) use ($app,$db){
 		$group = $db->group();
 
 		$members = array();
@@ -101,25 +172,28 @@ $app->group('/group', function () use ($app,$db){
 		));
 	});
 
-	$app->get('/:groupID/edit', function ($groupID){
-		echo "Group: ".$groupID." edit";
+	//group editing
+	$app->get('/:id/edit', function ($id){
+		echo "Group: ".$id." edit";
 	});
 });
 
 $app->group('/user', function () use ($app,$db){
 
-	$app->get('/add', function (){
-		$post = $app->request()->post();
+	//user adding
+	$app->get('/add', function () use ($app,$db){
+		$post = $app->request->post();
 		$result = $db->user()->insert($post);
 
 		if(!$result)
-			echo "Error!"
+			echo "Error!";
 		else
-			echo "User added"
+			echo "User added";
 	});
 	
-	$app->get('/:userID', function ($userID){
-		$user = $db->user()->where(id, $userID);
+	//user details
+	$app->get('/:id', function ($id) use ($app,$db){
+		$user = $db->user()->where('id', $id);
 
 		$groups = array();
 		foreach($user->membership() as $group){
@@ -132,8 +206,9 @@ $app->group('/user', function () use ($app,$db){
 		));
 	});
 
-	$app->get('/:userID/edit', function ($userID){
-		echo "User: ".$userID." edit";
+	//user editing
+	$app->get('/:id/edit', function ($id){
+		echo "User: ".$id." edit";
 	});
 });
 
