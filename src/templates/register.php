0f471a7c800cda2127c3ff5017a71d17cdfe8bb6
GrandiPL <jakub.wachowiak@gmail.com>
2013-12-04 00:10:28 +0000

    Added register file + basic html

---
+++ src/templates/register.php
@@ -0,0 +1,24 @@
+<style type="text/css" media="screen">
+	@import '../static/css/signin.css';
+</style>
+
+<div class="container">
+
+	<form class="form-signin">
+		<h2 class="form-signin-heading">Dolacz do nas juz dzis!</h2>
+		
+		<input type="text" class="form-control" placeholder="Nazwa uzytkownika*" required="" autofocus="">
+		<input type="text" class="form-control" placeholder="Adres Email*" required="" autofocus="">
+		<input type="text" class="form-control" placeholder="Imie">
+		<input type="text" class="form-control" placeholder="Nazwisko">
+		<input type="password" class="form-control" placeholder="Haslo*" required="">
+		
+		<table>
+		<tr>
+		<td><button class="btn btn-lg btn-primary btn-block" type="submit">Rejestruj</button></td>
+		<td><button class="btn btn-lg btn-primary btn-block" type="reset">Wyczysc</button></td>
+		</tr>
+		</table>
+		<h5>Pola oznaczone <font color="red">*</font> są wymagane !</h4>
+	</form>
+</div> <!-- /container -->
