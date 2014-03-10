0465f189341769ffccf6c704d54e7db11fd1a27d
Pejter <piotrluczak1995@gmail.com>
2014-03-10 17:04:45 +0100

    Fixed provisioning
    Fixed provisioning to work with Ubuntu 13.10.
    It also doesn't require default file

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
 sudo apt-get update
 sudo apt-get install -y curl git apache2 libapache2-mod-php5 php5-mcrypt php5-mysql
 rm -rf /var/www
 ln -fs /website /var/www
-sudo cp /vagrant/default /etc/apache2/sites-available/default
+sed -i '11s/None/All/g' /etc/apache2/sites-available/default
+echo "ServerName vagrant" > /etc/apache2/httpd.conf
 sudo a2enmod rewrite
 sudo service apache2 restart
